ec78cf39679f07e966e491234933fe2579cf0363
Gaurav0818 <Gaurav.ps.0818@gmail.com>
2023-04-05 19:17:08 +0530

    Creating First Raytraced Image
    Added a circle in centre

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,10 +4,23 @@
 #include "script/ray.h"
 #include "script/vec3.h"
 
+bool hit_sphere(const point3& center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b*b - 4*a*c;
+    
+    return discriminant > 0;
+}
+
 color ray_color(const ray& r)
 {
+    if(hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+    
     vec3 unit_direction = unit_vector(r.direction());
-
     const float t = 0.5f*(unit_direction.y() + 1.0f);
     return (1.0f - t) * color(1.0f) + t * color(0.5f, 0.7f, 1.0f);      // lerp ---- blendedValue = (1−t).startValue + t.endValue
 }
